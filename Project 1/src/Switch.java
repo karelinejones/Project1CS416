6686260c2a02197e43847917f4c9418eac04aabf
Raymond Gueta <guetara19@gmail.com>
2026-02-05 19:18:12 -0500

    Finish all methods of switch

--- Project 1/src/Switch.java
+++ Project 1/src/Switch.java
@@ -1,12 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
+import java.net.*;
 import java.util.HashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Switch {
     public static void main(String[] args) throws Exception, FileNotFoundException {
@@ -14,38 +9,80 @@ public class Switch {
         System.out.println("Mac Address: " + macAddress);
         File config = new File("Project 1/src/config.txt");
         Parser parser = new Parser(config);
+        Port switchPort = parser.parseMac(macAddress);
         HashMap<String, Port> neighbors = parser.getNeighbors(macAddress);
         HashMap<String, Integer> switchTable = new HashMap<>();
-        DatagramSocket socket = new DatagramSocket();
+        DatagramSocket socket = new DatagramSocket(switchPort.getUdpPort());
         DatagramPacket incoming = new DatagramPacket(new byte[1024], 1024);
 
-        while(true) {
-            socket.receive(incoming);
-            InetAddress incomingIp = incoming.getAddress();
-            String incomingMac = getMacFromIp(neighbors, incomingIp);
-            if (switchTable.containsKey(incomingMac)) {
 
+        //Main loop to receive and send out packets
+        while(true) {
+            //Receive the packet and create our own Packet object
+            socket.receive(incoming);
+            String frame = new String(incoming.getData(), 0, incoming.getLength());
+            Packet incomingPacket;
+            try {
+                incomingPacket = new Packet(frame);
+            } catch (Exception e) {
+                continue;
+            }
+
+            //If the destination address is in the switch table, find the neighbor with matching udp port and send to them
+            if(switchTable.containsKey(incomingPacket.getDestinationAddress())) {
+                byte[] outgoingByte = incomingPacket.createFrameString().getBytes();
+                for (String s : neighbors.keySet()) {
+                    Port p = neighbors.get(s);
+                    if (p.getUdpPort() == switchTable.get(incomingPacket.getDestinationAddress())) {
+                        DatagramPacket outgoingPacket = new DatagramPacket(
+                                outgoingByte,
+                                outgoingByte.length,
+                                p.getIpAddress(),
+                                p.getUdpPort()
+                        );
+                        socket.send(outgoingPacket);
+                        printSwitchTable(switchTable);
+                        System.out.println("Packet sent to " + s);
+                    }
+                }
             }
             else {
-                Port incomingPort = neighbors.get(incomingMac);
-                switchTable.put(macAddress, incomingPort.getUdpPort());
-
+                //Otherwise, update the switch table and flood to all neighbors excluding where the packet came from
+                switchTable.put(incomingPacket.getSourceAddress(), incoming.getPort());
+                printSwitchTable(switchTable);
+                flood(neighbors, socket, incoming.getPort(), incomingPacket);
             }
         }
     }
 
-    public static String getMacFromIp(HashMap<String, Port> neighbors, InetAddress ipAddress) {
-        for (String s : neighbors.keySet()) {
-            Port p = neighbors.get(s);
-            if (p.getIpAddress().equals(ipAddress)) {
-                return s;
-            }
+    public static void printSwitchTable(HashMap<String, Integer> switchTable) {
+        System.out.println("Switch Table:");
+        System.out.println("MAC Address\tPort");
+        for (String mac : switchTable.keySet()) {
+            System.out.println(mac + "\t" + switchTable.get(mac));
         }
-        return "None";
     }
 
-    public static void printSwitchTable() {
-
+    public static void flood(HashMap<String, Port> neighbors, DatagramSocket socket, int incomingPort, Packet packet) {
+        // Flood the frame to all neighbors except the incoming port
+        for (String mac : neighbors.keySet()) {
+            Port port = neighbors.get(mac);
+            if (port.getUdpPort() != incomingPort) {
+                byte[] outgoingByte = packet.createFrameString().getBytes();
+                try {
+                    DatagramPacket outgoingPacket = new DatagramPacket(
+                            outgoingByte,
+                            outgoingByte.length,
+                            port.getIpAddress(),
+                            port.getUdpPort()
+                    );
+                    socket.send(outgoingPacket);
+                } catch (Exception e) {
+                    System.err.println("Error flooding frame to " + mac + ": " + e.getMessage());
+                }
+            }
+        }
+        System.out.println("Frame flooded to all ports except incoming port " + incomingPort);
     }
 
 }
