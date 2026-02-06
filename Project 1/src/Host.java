ceadfb6ce515230098eb1ec9a126f3161fd90aff
karelinejones <lucky52603@gmail.com>
2026-02-05 19:30:35 -0500

    more host changes

--- Project 1/src/Host.java
+++ Project 1/src/Host.java
@@ -3,7 +3,6 @@ import java.net.*;
 import java.util.HashMap;
 import java.io.*;
 
-
 public class Host {
     private String macAddress;
     private InetAddress ipAddress;
@@ -15,8 +14,8 @@ public class Host {
         this.macAddress = macAddress;
         Parser parser = new Parser(config);
         Port portInfo = parser.parseMac(macAddress);
-        this.ipAddress = InetAddress.getByName(portInfo.getIpAddress());
-        this.port = Integer.parseInt(portInfo.getUdpPort());
+        this.ipAddress = (portInfo.getIpAddress());
+        this.port = (portInfo.getUdpPort());
         this.neighbors = parser.getNeighbors(macAddress);
     }
 
@@ -27,7 +26,7 @@ public class Host {
         System.out.println("Connected to: " + neighbors.keySet());
     }
 
-    public void send(String data, String destinationMac) throws IOException {
+    public void send (String data, String destinationMac) throws IOException {
         String frame = destinationMac + ":" + this.macAddress + ":" + data;
         Packet packet = new Packet(frame);
         System.out.println("[" + macAddress + "] Sending:");
@@ -37,15 +36,14 @@ public class Host {
 
         for (Port neighborPort : neighbors.values()) {
             byte[] sendData = frame.getBytes();
-            InetAddress destAddress = InetAddress.getByName(neighborPort.getIpAddress());
-            int destPort = Integer.parseInt(neighborPort.getUdpPort());
+            InetAddress destAddress = (neighborPort.getIpAddress());
+            int destPort = (neighborPort.getUdpPort());
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, destAddress, destPort);
             socket.send(sendPacket);
             System.out.println("  -> " + destAddress.getHostAddress() + ":" + destPort);
             break;
         }
     }
-
     public void receive() throws IOException {
         byte[] buffer = new byte[1024];
         DatagramPacket recvPacket = new DatagramPacket(buffer, buffer.length);
@@ -59,23 +57,69 @@ public class Host {
         System.out.println("  Dst: " + p.getDestinationAddress());
         System.out.println("  Data: " + p.getData());
     }
-
-    public String getMacAddress() {
-        return macAddress;
-    }
-
-    public InetAddress getIpAddress() {
-        return ipAddress;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
     public void close() {
         if (socket != null && !socket.isClosed()) {
             socket.close();
         }
+    }
+
+    public static void runHostA() throws Exception {
+        File config = new File("config.txt");
+        Host host = new Host("A", config);
+        host.start();
+        host.runInteractive();
+    }
+
+    public static void runHostB() throws Exception {
+        File config = new File("config.txt");
+        Host host = new Host("B", config);
+        host.start();
+        host.runInteractive();
+    }
+
+    public static void runHostC() throws Exception {
+        File config = new File("config.txt");
+        Host host = new Host("C", config);
+        host.start();
+        host.runInteractive();
+    }
+
+    public static void runHostD() throws Exception {
+        File config = new File("config.txt");
+        Host host = new Host("D", config);
+        host.start();
+        host.runInteractive();
+    }
+
+    private void runInteractive() {
+        Scanner keyboard = new Scanner(System.in);
+        while (true) {
+            System.out.println("\n1. Send\n2. Receive\n3. Exit");
+            System.out.print("Choice: ");
+            String choice = keyboard.nextLine();
+
+            if (choice.equals("1")) {
+                System.out.print("Destination (A/B/C/D): ");
+                String dest = keyboard.nextLine().toUpperCase();
+                System.out.print("Message: ");
+                String msg = keyboard.nextLine();
+                try {
+                    send(msg, dest);
+                } catch (IOException e) {
+                    System.err.println("Error sending: " + e.getMessage());
+                }
+            } else if (choice.equals("2")) {
+                try {
+                    receive();
+                } catch (IOException e) {
+                    System.err.println("Error receiving: " + e.getMessage());
+                }
+            } else if (choice.equals("3")) {
+                close();
+                break;
+            }
+        }
+        keyboard.close();
     }
 
     public static void main(String[] args) throws Exception {
@@ -85,7 +129,7 @@ public class Host {
         }
 
         String macAddress = args[0];
-        File config = new File("Project 1/src/config.txt");
+        File config = new File("config.txt");
         Host host = new Host(macAddress, config);
         host.start();
 
